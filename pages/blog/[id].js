11e1690b3b4ec66cde6616819d3fc3816a4a4275
santhalakshminarayana <santhalakshminarayana@gmail.com>
2021-09-20 00:17:49 +0530

    added color theory blog

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -21,86 +21,122 @@ const em_background = c1 + '26', em_color = c1 + 'f2';
 const strong_background = c5 + '26', strong_color = c4 + 'f2';
 const hr_color = c1 + '80', blockquote_background_color = c3 + '26', blockquote_border = c3 + 'e6';
 
+function getHeadingAnchorProps(props) {
+	let id = "";
+	let modifiedProps = {};
+	if (typeof(props.children) === 'string') 
+		modifiedProps['children'] = [props.children];
+	else
+		modifiedProps['children'] = props.children;
+
+	let split_array = modifiedProps.children[modifiedProps.children.length - 1].split(':=:');
+	if (split_array.length > 1) {
+		id = split_array[1];
+		modifiedProps.children[modifiedProps.children.length - 1] = split_array[0];
+	}
+
+	return [modifiedProps, id];
+}
+
 const components = {
 	img: props => <NextOptimizedImage img_props = {props}/>,
 
-	h1: props => <h1 style = {{ fontFamily: "'Ubuntu', sans-serif", 
-															fontSize: 'calc(1rem + 1.5vw)', 
-															color: h1_color,
-															margin: '1vh 0 1vh 0',
-															overflowWrap: 'break-word', }} 
-									{...props} />,
+	h1: props => {
+		const [modifiedProps, id] = getHeadingAnchorProps(props);
+		return (<h1 
+			style = {{ fontFamily: "'Ubuntu', sans-serif", 
+				fontSize: 'calc(1rem + 1.5vw)', 
+				color: h1_color,
+				margin: '1vh 0 1vh 0',
+				overflowWrap: 'break-word', }} 
+			id = {id}
+			{...modifiedProps} />);},
 
-	h2: props => <h2 style = {{ fontFamily: "'Maven Pro', sans-serif", 
-															fontSize: 'calc(1rem + 1vw)', 
-															color: h2_color,
-															margin: '1vh 0 1vh 0',
-															overflowWrap: 'break-word', }} 
-										{...props} />,
+	h2: props => {
+		const [modifiedProps, id] = getHeadingAnchorProps(props);
+		return (<h2 
+			style = {{ fontFamily: "'Maven Pro', sans-serif", 
+				fontSize: 'calc(1rem + 1vw)', 
+				color: h2_color,
+				margin: '1vh 0 1vh 0',
+				overflowWrap: 'break-word', }}
+			id = {id} 
+			{...modifiedProps} />);},
 
-	h3: props => <h3 style = {{ fontFamily: "'Maven Pro', sans-serif", 
-															fontSize: 'calc(1rem + 0.5vw)', 
-															color: h3_color,
-															margin: '1vh 0 1vh 0',
-															overflowWrap: 'break-word', }} 
-									{...props} />,
+	h3: props => {
+		const [modifiedProps, id] = getHeadingAnchorProps(props);
+		return (<h3 
+			style = {{ 
+				fontFamily: "'Maven Pro', sans-serif", 
+				fontSize: 'calc(1rem + 0.5vw)', 
+				color: h3_color,
+				margin: '1vh 0 1vh 0',
+				overflowWrap: 'break-word', }} 
+			id = {id}
+			{...modifiedProps} />);},
 
-	p: props => <p style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
-														fontSize: 'calc(1rem + 0.1vw)', 
-														color: p_color,
-														margin: '0vh 0 1vh 0',
-														overflowWrap: 'break-word', }} 
-									{...props} />,
+	p: props => <p 
+		style = {{ 
+			fontFamily: "'Source Sans Pro', sans-serif", 
+			fontSize: 'calc(1rem + 0.1vw)', 
+			color: p_color,
+			margin: '0vh 0 1vh 0',
+			overflowWrap: 'break-word', }} 
+		{...props} />,
 	
-	ul: props => <ul style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
-														fontSize: 'calc(1rem + 0.1vw)', 
-														color: ul_color,
-														margin: '1vh 0 1vh calc(2vw)',
-														overflowWrap: 'break-word', }} 
-									{...props} />,
+	ul: props => <ul 
+		style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
+			fontSize: 'calc(1rem + 0.1vw)', 
+			color: ul_color,
+			margin: '1vh 0 1vh calc(2vw)',
+			overflowWrap: 'break-word', }} 
+		{...props} />,
 
-	ol: props => <ol style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
-													fontSize: 'calc(1rem + 0.1vw)', 
-													color: ol_color,
-													margin: '1vh 0 1vh calc(2vw)',
-													overflowWrap: 'break-word', }} 
-								{...props} />,
+	ol: props => <ol 
+		style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
+			fontSize: 'calc(1rem + 0.1vw)', 
+			color: ol_color,
+			margin: '1vh 0 1vh calc(2vw)',
+			overflowWrap: 'break-word', }} 
+		{...props} />,
 
 	a: props => <HoverableLink link_props = {props} />,
 
 	code: props => <CodeSyntaxHighlighter code_props = {props} />,
 
-	em: props => <em style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
-													fontSize: 'calc(1rem + 0.1vw)', 
-													fontStyle: 'normal',
-													backgroundColor: em_background,
-													color: em_color,
-													margin: '0vh 0 1vh 0',
-													padding: '0 2px 0 2px',
-													borderRadius: '2px' }} 
-								{...props} />,
+	em: props => <em 
+		style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
+			fontSize: 'calc(1rem + 0.1vw)', 
+			fontStyle: 'normal',
+			backgroundColor: em_background,
+			color: em_color,
+			margin: '0vh 0 1vh 0',
+			padding: '0 2px 0 2px',
+			borderRadius: '2px', }} 
+		{...props} />,
 
-	strong: props => <strong style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
-												fontSize: 'calc(1rem + 0.1vw)', 
-												fontStyle: 'normal',
-												//fontWeight: 'normal',
-												fontWeight: 'bold',
-												//backgroundColor: strong_background,
-												color: strong_color,
-												margin: '0vh 0 1vh 0',
-												//padding: '0 2px 0 2px',
-												//borderRadius: '2px',
-												overflowWrap: 'break-word', }} 
-							{...props} />,
+	strong: props => <strong 
+		style = {{ fontFamily: "'Source Sans Pro', sans-serif", 
+			fontSize: 'calc(1rem + 0.1vw)', 
+			fontStyle: 'normal',
+			fontWeight: 'bold',
+			color: strong_color,
+			margin: '0vh 0 1vh 0',
+			overflowWrap: 'break-word', }} 
+		{...props} />,
 
-	hr: _ => <hr style = {{ margin: '2vh 25% 2vh 25%',
-    											border: `1px solid ${hr_color}`, }} />,
+	hr: _ => <hr 
+		style = {{ margin: '2vh 25% 2vh 25%',
+    	border: `1px solid ${hr_color}`, }} />,
 
-	blockquote: props => <blockquote style = {{ backgroundColor: blockquote_background_color, 
-																						padding: '10px',
-																						margin: '1vh 0 1vh 0',
-																						borderLeft: `5px solid ${blockquote_border}`, }} 
-								{...props} />,
+	blockquote: props => <blockquote 
+		style = {{ backgroundColor: blockquote_background_color, 
+			padding: '10px',
+			margin: '1vh 0 1vh 0',
+			borderLeft: `5px solid ${blockquote_border}`, 
+		}} 
+		{...props} 
+	/>,
 }
 
 const home_page_url = 'https://santhalakshminarayana.github.io/';
